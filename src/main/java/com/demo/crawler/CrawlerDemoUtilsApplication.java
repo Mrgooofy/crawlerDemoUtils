f0ade84b3e6a6e799989df9136c0fb3d63fd7511
goofy <goofy@xx.com>
2021-12-18 12:07:13 +0800

    init

---
+++ src/main/java/com/demo/crawler/CrawlerDemoUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerDemoUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlerDemoUtilsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/crawler/CrawlerDemoUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.crawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlerDemoUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
